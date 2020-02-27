f4d9034ca00445526b6f0cbeeb7863563cd3ec84
juhyung <sonju0427@gmail.com>
2020-02-28 01:26:44 +0900

    #2 stn network lambda layer
    x= keras.layers.Lambda(_transform,
                               output_shape=stn_input_output_shape)([x, localization_net(x)]) 레이어에서 미지원 함수로 에러남. 
    node lambda/AddN of type AddN cannot be converted, fall back to tf2onnx
    Traceback (most recent call last):
      File "build_model.py", line 66, in <module>
        onnx_model = keras2onnx.convert_keras(model, model.name)
      File "/usr/local/lib/python3.6/dist-packages/keras2onnx/main.py", line 60, in convert_keras
        parse_graph(topology, tf_graph, target_opset, output_names, output_dict)
      File "/usr/local/lib/python3.6/dist-packages/keras2onnx/parser.py", line 794, in parse_graph
        graph, keras_node_dict, topo, top_level, output_names)
      File "/usr/local/lib/python3.6/dist-packages/keras2onnx/parser.py", line 595, in _parse_graph_core
        _on_parsing_tf_subgraph(graph, nodes, varset)
      File "/usr/local/lib/python3.6/dist-packages/keras2onnx/parser.py", line 335, in _on_parsing_tf_subgraph
        raise RuntimeError("Some tensorflow operation doesn't support, stop converting.")
    RuntimeError: Some tensorflow operation doesn't support, stop converting.

--- keras_ocr_onnx/build_model.py
+++ keras_ocr_onnx/build_model.py
@@ -5,11 +5,21 @@ import numpy as np
 import keras2onnx
 from util_rec import _transform
 
-# (64, 128, 256, 256, 512, 512, 512)
+"""
+    'height': 31,
+    'width': 200,
+    'color': False,
+    'filters': (64, 128, 256, 256, 512, 512, 512),
+    'rnn_units': (128, 128),
+    'dropout': 0.25,
+    'rnn_steps_to_discard': 2,
+    'pool_size': 2,
+    'stn': True,
+"""
 
 def build_model(alphabet, height, width, color, filters, rnn_units, dropout,
                 rnn_steps_to_discard, pool_size, stn=True):
-    inputs = keras.layers.Input((height, width, 3 if color else 1), batch_size=4)
+    inputs = keras.layers.Input((height, width, 3 if color else 1), batch_size=1)
     x = keras.layers.Permute((2, 1, 3))(inputs)
     # x = keras.layers.Lambda(lambda x: x[:, :, ::-1])(x)
     # x = x[..., ::-1]
@@ -17,17 +27,17 @@ def build_model(alphabet, height, width, color, filters, rnn_units, dropout,
     x = keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='conv_2')(x)
     x = keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='conv_3')(x)
     x = keras.layers.BatchNormalization(name='bn_3')(x)
-    x = keras.layers.MaxPooling2D(pool_size=(3,  3), name='maxpool_3')(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2,  2), name='maxpool_3')(x)
     x = keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='conv_4')(x)
     x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_5')(x)
     x = keras.layers.BatchNormalization(name='bn_5')(x)
-    x = keras.layers.MaxPooling2D(pool_size=(3, 3), name='maxpool_5')(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2), name='maxpool_5')(x)
     x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_6')(x)
     x = keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_7')(x)
     x = keras.layers.BatchNormalization(name='bn_7')(x)
 
     stn_input_output_shape = (width // pool_size**2, height // pool_size**2, 512)
-    stn_input_layer = keras.layers.Input(stn_input_output_shape, batch_size=4)
+    stn_input_layer = keras.layers.Input(stn_input_output_shape, batch_size=1)
     locnet_y = keras.layers.Conv2D(16, (5, 5), padding='same', activation='relu')(stn_input_layer)
     locnet_y = keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu')(locnet_y)
     locnet_y = keras.layers.Flatten()(locnet_y)
@@ -40,9 +50,9 @@ def build_model(alphabet, height, width, color, filters, rnn_units, dropout,
 
     x= keras.layers.Lambda(_transform,
                            output_shape=stn_input_output_shape)([x, localization_net(x)])
-    x = keras.layers.Reshape(target_shape=(width // pool_size**2,
-                                           (height // pool_size ** 2) * 512),
-                            name='reshape')(x)
+    # x = keras.layers.Reshape(target_shape=(width // pool_size**2,
+    #                                        (height // pool_size ** 2) * 512),
+    #                         name='reshape')(x)
     # x = keras.layers.Dense(4)(x)
     x = keras.models.Model(inputs=inputs, outputs=x)
     return x
@@ -51,7 +61,7 @@ def build_model(alphabet, height, width, color, filters, rnn_units, dropout,
 if __name__ == '__main__':
     import os
 
-    model = build_model(1,512, 512, 3, 7, 2, 0.1, 2, 4)
+    model = build_model(1,31, 200, 1, 7, 2, 0.1, 2, 2)
     # model.summary()
     onnx_model = keras2onnx.convert_keras(model, model.name)
 
@@ -59,4 +69,4 @@ if __name__ == '__main__':
 
     print('ocr2onnx DONE')
 
-    os.system('./onnx2trt model.onnx -o model.trt -d 16 -b 4')
+    # os.system('./onnx2trt model.onnx -o model.trt -d 16 -b 4')
